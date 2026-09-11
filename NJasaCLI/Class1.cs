678f4d5263d95bb03fd3bc7167f055f887970377
HajosBB <hajosb@kkszki.hu>
2026-09-11 12:24:41 +0200

    Add project files.

---
+++ NJasaCLI/Class1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Globalization;
+
+namespace NJasaCLI
+{
+
+    
+        public class beolvasas
+        {
+            public string nev { get; set; }
+            public int ev { get; set; }
+            public string celpont { get; set; }
+            public int legenyseg { get; set; }
+            public bool sikeres { get; set; }
+            public string leiras { get; set; }
+            public double koltseg { get; set; }
+            public double hasznosteher { get; set; }
+            public string kockazatiszint { get; set; }
+
+            public beolvasas(string sor)
+            {
+                string[] adatok = sor.Split(';');
+                this.nev = adatok[0];
+                this.ev = int.Parse(adatok[1]);
+                this.celpont = adatok[2];
+                this.legenyseg = int.Parse(adatok[3]);
+            var sikeres = adatok[4];
+            if (string.Equals(sikeres, "Igen")) this.sikeres = true;
+            else if (string.Equals(sikeres, "Nem")) this.sikeres = false;
+                this.leiras = adatok[5];
+                this.koltseg = double.Parse(adatok[6], CultureInfo.GetCultureInfo("hu-HU"));
+                this.hasznosteher = double.Parse(adatok[7], CultureInfo.GetCultureInfo("hu-HU"));
+
+                
+            }
+        
+
+    }
+}
---
+++ NJasaCLI/Program.cs
@@ -0,0 +1,59 @@
+﻿using System.IO;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System;
+using System.Linq;
+
+namespace NJasaCLI
+{
+    internal class Program
+    {
+        static List<beolvasas> beolvasasok = new List<beolvasas>();
+
+        static void Main(string[] args)
+        {
+            using (var sr = new StreamReader("NASAmissions.txt"))
+            {
+                sr.ReadLine();
+                while (!sr.EndOfStream)
+                {
+                    beolvasasok.Add(new beolvasas(sr.ReadLine()));
+                }
+                sr.Close();
+            }
+            //feladat 3
+            int kuldetesekszama = beolvasasok.Count;
+            Console.WriteLine("{0} kuldetes talalhato az allomanyban.", kuldetesekszama);
+
+            // feladat 4
+            Console.Write("adjon meg egy kuldetest: ");
+            string bekeres = Console.ReadLine();
+            var matches = beolvasasok.Where(x => x.nev != null && x.nev.IndexOf(bekeres, StringComparison.OrdinalIgnoreCase) >= 0).ToList();
+            if (matches.Count > 0)
+            {
+                foreach (var m in matches)
+                {
+                    Console.WriteLine("------------------------------");
+                    Console.WriteLine("Nev: {0}", m.nev);
+                    Console.WriteLine("Ev: {0}", m.ev);
+                    Console.WriteLine("Celpont: {0}", m.celpont);
+                    Console.WriteLine("Legenyseg: {0}", m.legenyseg);
+                    Console.WriteLine("Sikeres: {0}", m.sikeres);
+                    Console.WriteLine("Leiras: {0}", m.leiras);
+                    Console.WriteLine("Koltseg: {0}", m.koltseg);
+                    Console.WriteLine("Hasznosteher: {0}", m.hasznosteher);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Nincs talalat.");
+            }
+            // feladat 5 
+
+
+        }
+        
+     }
+}
+
+
